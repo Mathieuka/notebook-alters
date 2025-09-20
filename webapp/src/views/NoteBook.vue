20d0573fcc025adff25926f1c10d98c46b49e514
Mathieu Kahlaoui <34446722+Mathieuka@users.noreply.github.com>
2025-09-20 20:28:26 +0200

    style: work in progress

--- webapp/src/views/NoteBook.vue
+++ webapp/src/views/NoteBook.vue
@@ -29,7 +29,43 @@ onMounted(async () => {
 <template>
   <div class="container">
     <!-- <editor-content :editor="editor" /> -->
+    <input type="text" />
+    <div class="box-container">
+      <div class="box">Value</div>
+      <div class="box">Value</div>
+      <div class="box">Value</div>
+      <div class="box">Value</div>
+    </div>
   </div>
 </template>
 
-<style></style>
+<style>
+.container {
+  flex-direction: column;
+  width: 100%;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border: 3px solid #ccc;
+  border-radius: 8px;
+}
+
+.box-container {
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  width: 600px;
+
+  background-color: #f0f0f0;
+  border: 1px solid #ccc;
+  border-radius: 8px;
+  gap: 10px;
+}
+
+.box {
+  min-height: 100px;
+  background-color: #f0f0f0;
+  border: 1px solid #ccc;
+  border-radius: 8px;
+}
+</style>
